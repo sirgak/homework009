d77a31faa335a1b97f88ee5aa2db08f1f76da2bd
sirgak <life_4_ever@mail.ru>
2022-05-20 21:02:07 +0300

    test
    test

---
+++ homework004/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace homework004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("До перестановки");
+            string name ;
+            string surname;
+            name = "Иванов";
+            surname = "Иван";
+            Console.WriteLine("Имя " + name);
+            Console.WriteLine("Фамилия " + surname);
+            Console.WriteLine("После перестановки");
+            (name, surname) = (surname, name);
+            Console.WriteLine("Имя " + name);
+            Console.WriteLine("фамилия " + surname);
+        }
+    }
+}
